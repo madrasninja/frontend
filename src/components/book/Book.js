0f8419d93b0e496e1294187191865a285560d3eb
Sivachandran <sivan@jaishu.com>
2019-02-11 18:42:20 +0530

    added success message

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -7,14 +7,33 @@ import standingNinja from "./../../assets/img/standing-ninja.png";
 import './Book.css';
 import ServiceForm from "./form";
 import { bookService } from "./../../services/BookService";
+import Notifier from "../notifier/Notifier";
 
 class Book extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            responseStatus: false
+        }
+    }
+
     bookaService(values) {
         values.Session_Time = {
             From: values.serviceDate + values.serviceTime,
             To: moment(values.serviceDate).hours(values.serviceTime.split(':')[0]).minutes(values.serviceTime.split(':')[1]).add(values.serviceHours, 'h')
         }
         this.props.bookService(values);
+    }
+    componentWillReceiveProps(nextProps) {
+        if (this.props.formResponse !== nextProps.formResponse) {
+            let {message, response} = nextProps.formResponse.data;
+            if (response === 'success') {
+                this.setState({
+                    responseStatus: true,
+                    message
+                })
+            }
+        }
     }
     render() {
         return (
@@ -25,11 +44,12 @@ class Book extends Component {
                 <Col xs={12}>
                     <Row>
                         <Col xs={12} sm={3} md={6} className="text-center ninja">
-                            <img src={standingNinja} alt="ninja" />
+                            <img src={standingNinja} alt="ninja"/>
                         </Col>
                         <Col xs={12} sm={9} md={{size: 4, offset: 1}}>
                             <h4 className="text-center font-weight-light">Fill up & get ninjafied</h4>
                             <hr/>
+                            <Notifier message={this.state.message} color="success" show={this.state.responseStatus}/>
                             <ServiceForm getValues={(values)=>this.bookaService(values)}/>
                         </Col>
                     </Row>
