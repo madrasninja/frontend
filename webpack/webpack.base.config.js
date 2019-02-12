36e362a41e338fc1055b6e35cd6637bded18c428
Sivachandran <sivan@jaishu.com>
2019-02-12 16:24:52 +0530

    favicon config

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -53,7 +53,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
+            favicon: 'favicon.png'
         })
     ]
 };
