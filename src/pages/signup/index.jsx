3f071cb59fd0c98cae91e491acb04068d25947a5
Sivachandran Raina <sivachandran94@gmail.com>
2019-03-15 17:57:36 +0530

    success msg for signup

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
-import cookie from "react-cookies";
 import { Link } from "react-router-dom";
 
 import SignupForm from "./form";
@@ -15,12 +14,12 @@ export default class Signup extends Component {
     state = {
         notifier: false,
         color: 'primary',
-        message: ''
+        message: '',
+        success: false
     }
 
     apicall = values => {
         API_CALL('post', 'signup', values, null, data => {
-            console.log(data);
             const { type, message } = data;
             if (type == 2) {
                 this.setState({
@@ -29,7 +28,9 @@ export default class Signup extends Component {
                     message: 'Hey ' + values.First_Name + ', you are already entitled. We have emailed you the link, can setup password there.'
                 })
             } else if (type == 1) {
-
+                this.setState({
+                    success: true
+                })
             } else {
                 this.setState({
                     notifier: true,
@@ -40,25 +41,40 @@ export default class Signup extends Component {
         })
     }
     render() {
-        const { notifier, color, message } = this.state;
-        return (
-            <div className="login">
-                <Row>
-                    <Col xs={12} className="text-center">
-                        <h3 className="">
-                            Ready to get ninjafied
-                        </h3>
-                        <p className="text-black-50">One account for all the services</p>
-                    </Col>
-                    <Col xs={12} md={{ size: 4, offset: 4 }}>
-                        <SignupForm getValues={(data) => this.apicall(data)} />
-                        <Notifier show={notifier} color={color} message={message} />
-                    </Col>
-                    <Col xs={12} className="add-on text-center">
-                        Already have an account? <Link to="/signin">Signin</Link>
-                    </Col>
-                </Row>
-            </div>
-        );
+        const { notifier, color, message, success } = this.state;
+        if (success) {
+            return (
+                <div className="login">
+                    <Row>
+                        <Col xs={12} className="text-center">
+                            <h3>
+                                You are almost done!
+                            </h3>
+                            <p className="text-black-50">Please verify your email & get ninjafied officially</p>
+                        </Col>
+                    </Row>
+                </div>
+            )
+        } else {
+            return (
+                <div className="login">
+                    <Row>
+                        <Col xs={12} className="text-center">
+                            <h3 className="">
+                                Ready to get ninjafied
+                            </h3>
+                            <p className="text-black-50">One account for all the services</p>
+                        </Col>
+                        <Col xs={12} md={{ size: 4, offset: 4 }}>
+                            <SignupForm getValues={(data) => this.apicall(data)} />
+                            <Notifier show={notifier} color={color} message={message} />
+                        </Col>
+                        <Col xs={12} className="add-on text-center">
+                            Already have an account? <Link to="/signin">Signin</Link>
+                        </Col>
+                    </Row>
+                </div>
+            );
+        }
     }
 }
