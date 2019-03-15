ce0c53a1561cc224941446298301d30df94e229e
Sivachandran Raina <sivachandran94@gmail.com>
2019-03-15 16:43:07 +0530

    one another

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config');
 const Visualizer = require('webpack-visualizer-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(base, {
     mode: 'production',
@@ -11,5 +12,12 @@ module.exports = merge(base, {
         new Visualizer({
             filename: './statistics.html' // statistics.html will view the bundle.js usages.
         }),
+        new CopyPlugin([
+            {
+              from: '/_redirects',
+              to: '../dist',
+              toType: 'file',
+            },
+        ]),
     ],
 });
